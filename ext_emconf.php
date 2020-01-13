335a4f4bd3eb4baba592265fecef8368c82ee8f2
Jan Stockfisch <j.stockfisch@neusta.de>
2020-01-13 10:23:55 +0100

    [TASK] Remove TYPO3 v7 support from emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,7 +18,7 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'extension',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-8.99.99',
+            'typo3' => '8.7.0-8.99.99',
         ],
         'conflicts' => [
         ],
